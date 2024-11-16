9c41d2679275967c821b84d7161524507aa2e2c0
shunki-tada <shunki.tada@dena.jp>
2024-11-17 08:34:07 +0900

    差分削除

--- SwiftUIProject/NeighborNagura/ViewModel/Unity.swift
+++ SwiftUIProject/NeighborNagura/ViewModel/Unity.swift
@@ -108,12 +108,7 @@ class Unity: SetsNativeState, ObservableObject  {
 
     private func stateDidSet() {
         // x と zを入れ替えている
-        let nativeState = NativeState(
-            x: ballAcceleration.z,
-            y: ballAcceleration.y,
-            z: ballAcceleration.x,
-            userRole: Int32(userRole.rawValue)
-        )
+        let nativeState = NativeState(x: z, y: y, z: x, userRole: Int32(userRole.rawValue))
         setNativeState?(nativeState)
     }
 
